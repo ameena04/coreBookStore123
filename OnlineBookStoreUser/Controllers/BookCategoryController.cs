1780eeef1a2ea2ea6cb69bb232c39ee349912221
Ameena Bano <Ameena.Bano@NIIT-Tech.com>
2019-04-03 15:39:00 +0530

    Add project files.

---
+++ OnlineBookStoreUser/Controllers/BookCategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookStoreUser.Models;
+
+namespace OnlineBookStoreUser.Controllers
+{
+    public class BookCategoryController : Controller
+    {
+        Book_Store_DbContext context = new Book_Store_DbContext();
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ OnlineBookStoreUser/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineBookStoreUser.Models;
+
+namespace OnlineBookStoreUser.Controllers
+{
+    public class BookController : Controller
+    {
+        Book_Store_DbContext context = new Book_Store_DbContext();
+        public IActionResult PublicationIndex()
+        {
+            var publicationList = context.Books.ToList();
+            return View(publicationList);
+        }
+    }
+}
